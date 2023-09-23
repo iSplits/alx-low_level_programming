94b8b788a9fa62904a948da9deae2b51b3773d15
iSplits <hauntargurlzheed@gmail.com>
2023-09-23 07:48:48 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - the main function of the program
+ *
+ * Return: to 0 means success
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
