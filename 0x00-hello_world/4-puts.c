702d9450ba6ad1e73827ead8fedfb532580612e3
iSplits <hauntargurlzheed@gmail.com>
2023-09-23 07:17:35 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-	puts("Programming is like building a multilingual puzzle,\n");
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle,");
 	return 0;
 }
