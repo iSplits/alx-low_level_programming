7ab35c3dccfdb7f066f95565d74b01ca353e3101
iSplits <hauntargurlzheed@gmail.com>
2023-09-24 14:08:31 -0700

    lowerUPPERcAsE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet then upper case in new line.
+ *
+ * Return:has to be 0 to be a Success
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
