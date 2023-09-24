950e227435be2681c32f7547450b73d4cc2acbe3
iSplits <hauntargurlzheed@gmail.com>
2023-09-24 14:26:01 -0700

    Single line after base 10 single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers between of base 10 starting from 0.
+ *
+ * Return: to 0 means success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
